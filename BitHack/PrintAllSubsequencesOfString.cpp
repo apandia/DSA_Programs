84a824e96911e9c9eb370db0a3a9bfc8e97a2085
apandia <96688353+apandia@users.noreply.github.com>
2022-06-27 17:07:31 +0530

    Create PrintAllSubsequencesOfString.cpp

---
+++ BitHack/PrintAllSubsequencesOfString.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Print all subsequences of a string
+/*
+Input string = "abc"
+All possible substring of abc =
+        --> empty substring
+a
+b
+ab
+c
+ac
+bc
+abc
+*/
+
+// Approach1: Using bit method (Power Set)
+//TC: O(2^n * n)    [2^n for outer loop and n for inner loop]
+//SC: O(1)
+void printAllSubsequencePowerSet(string str)
+{
+    int n = str.size();
+
+    for(int num = 0; num < (1<<n); num++)
+    {
+        string substring;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(num & (1<<i))
+                substring += str[i];
+        }
+        cout<<substring<<endl;
+    }
+}
+
+// Approach2: Using recursion
+//TC: O(2^n)
+//SC: O(N)      [Recursive stack space
+void printAllSubsequence(string str, int index, string& substring)
+{
+    if(index == str.length())
+    {
+        cout<<"{"<<substring<<"}"<<endl;
+        return;
+    }
+
+    substring += str[index];
+    printAllSubsequence(str, index+1, substring);
+    substring.pop_back();
+    printAllSubsequence(str, index+1, substring);
+}
+
+int main()
+{
+    string str = "abc";
+
+    cout<<"All possible substring of "<<str<<" = "<<endl;
+
+    printAllSubsequencePowerSet(str);
+
+    cout<<"All possible substring of "<<str<<" = "<<endl;
+
+    string tmp;
+    printAllSubsequence(str, 0, tmp);
+
+    return 0;
+}
