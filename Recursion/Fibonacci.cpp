81692a6631c43ee7e3c14d7e792e7ae70339a0f5
apandia <96688353+apandia@users.noreply.github.com>
2022-07-23 14:07:53 +0530

    Update Fibonacci.cpp

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -5,6 +5,14 @@ using namespace std;
 //In each level number of calls made = 1+2+4+8... (GP).
 //Firt call for (n-1) and (n-2) will be made say we call them as l1(n-1) and l2(n-2)
 //In next level again opeation with -1 and -2 will be made for l1 and l2 it means (n-1-1 and n-1-2) for l1 and (n-2-1) and (n-2-2) for l2 making 4 callls.
+/*
+                f(5)                    1   (2^0)
+              /      \
+            f(4)     f(3)               2   (2^1)
+           /   \    /   \
+         f(3) f(2) f(2) f(1)            4   (2^2)   etc....                      
+*/
+
 //TC - O(2^N)
 //SC - O(N)
 int Fibonaci(int n)
